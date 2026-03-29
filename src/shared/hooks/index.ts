7bf443940b9d8241bb75a3fe0fbc3e5dea03e1ca
SCANDAL9 <duglasrivero2@gmail.com>
2026-03-29 10:51:01 -0400

    Exporta hooks compartidos

---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useTableManager } from './useTableManager'
+export { useModal } from './useModal'
+export type { TableColumn, TableOptions, UseTableManagerReturn } from './useTableManager'
+export type { UseModalReturn } from './useModal'
