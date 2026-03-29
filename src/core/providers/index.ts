4f50d11ed7a19201e23dff91572ebf704bdf5723
SCANDAL9 <duglasrivero2@gmail.com>
2026-03-29 10:50:35 -0400

    Exporta proveedores core

---
+++ src/core/providers/index.ts
@@ -0,0 +1,4 @@
+export { QueryProvider } from './QueryProvider'
+export { ThemeProvider, useTheme } from './ThemeProvider'
+export { useAppStore } from '../store/appStore'
+export type { AppState } from '../store/appStore'
