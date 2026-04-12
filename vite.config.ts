09f2d94475442275e10103f8af04481a34e2e25a
SCANDAL9 <duglasrivero2@gmail.com>
2026-04-11 20:37:18 -0400

    config: Vite y Docker

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    host: '0.0.0.0', // Escuchar en todas las interfaces (necesario en Docker)
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
